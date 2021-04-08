4aa3e9f4e01c3b623a41a90676d66f75c03504b5
Eric Ford <ericford@mac.com>
2021-04-08 19:33:13 -0400

    Linting.

--- app/backend/api/src/orm/orm.py
+++ app/backend/api/src/orm/orm.py
@@ -1,66 +1,69 @@
-import psycopg2
-import os
-
-from distutils.util import strtobool
-from .context import api, settings
-import settings as settings
-from api.src.db import drop_all_tables, insert_records, retrieve_record, retrieve_records
+from api.src.db import drop_all_tables, insert_records, \
+    retrieve_record, retrieve_records
 
 
-TABLES = ['areacodes', 
-          'zipcodes', 
-          'cities', 
-          'merchants', 
-          'areacodes_zipcodes', 
-          'cities_zipcodes'
-         ]
+TABLES = ['areacodes',
+          'zipcodes',
+          'cities',
+          'merchants',
+          'receipts',
+          'areacodes_zipcodes',
+          'cities_zipcodes',
+          ]
 
-def build_from_record(This_class, record):
+
+def build_from_record(cls, record):
     """
-    Given a record returned from the DB, build an object of 
-    class This_class with attributes based on that record.
+    Given a record returned from the DB, build an object of
+    class cls with attributes based on that record.
     """
-    if not record: 
+    if not record:
         return None
-    attr = dict(zip(This_class.columns, record))
-    obj = This_class()
+    attr = dict(zip(cls.columns, record))
+    obj = cls()
     obj.__dict__ = attr
     return obj
 
-def build_from_records(This_class, records):
+
+def build_from_records(cls, records):
     """
-    Given records returned from the DB, build a list of 
-    objects of This_class with attributes based on that record.
+    Given records returned from the DB, build a list of
+    objects of cls with attributes based on that record.
     """
-    return [build_from_record(This_class, record) for record in records]
+    return [build_from_record(cls, record) for record in records]
+
 
 def clear_db():
     drop_all_tables()
 
-def find_all(This_class):
-    """Get all records for This_class and return This_class objects."""
-    records = retrieve_records(This_class.__table__)
-    return [build_from_record(This_class, record) for record in records]
 
-def find_by_id(This_class, input_id):
+def find_all(cls):
+    """Get all records for cls and return cls objects."""
+    records = retrieve_records(cls.__table__)
+    return [build_from_record(cls, record) for record in records]
+
+
+def find_by_id(cls, input_id):
     """
-    Retrieve record by id from DB, create and return obj of type This_class
+    Retrieve record by id from DB, create and return obj of type cls
     with values from that record.
     """
-    record = retrieve_record(This_class.__table__, 'id', input_id)
-    return build_from_record(This_class, record)
+    record = retrieve_record(cls.__table__, 'id', input_id)
+    return build_from_record(cls, record)
 
-def find_by_name(This_class, name):
+
+def find_by_name(cls, name):
     """
-    Retrieve record by name from DB, create and return obj of type This_class
+    Retrieve record by name from DB, create and return obj of type cls
     with values from that record.
     """
-    record = retrieve_record(This_class.__table__, 'name', name)
-    return build_from_record(This_class, record)
+    record = retrieve_record(cls.__table__, 'name', name)
+    return build_from_record(cls, record)
+
 
 def find_or_create(obj):
     """
-    Save values in input obj into DB. Return a *list* of *new* 
+    Save values in input obj into DB. Return a *list* of *new*
     objects of same type.
     """
     records = insert_records(obj.__table__, values(obj), keys(obj))
@@ -68,13 +71,16 @@ def find_or_create(obj):
     result = build_from_records(type(obj), records)
     return result
 
+
 def values(obj):
     """Return a list of values from the __dict__ in obj."""
     obj_attrs = obj.__dict__
-    return [obj_attrs[attr] for attr in obj.columns if attr in obj_attrs.keys()]
+    return [obj_attrs[attr] for attr in obj.columns
+            if attr in obj_attrs.keys()]
+
 
 def keys(obj):
     """Return a list of values from the __dict__ in obj."""
     obj_attrs = obj.__dict__
-    return [attr for attr in obj.columns if attr in obj_attrs.keys()]
-
+    return [attr for attr in obj.columns
+            if attr in obj_attrs.keys()]
